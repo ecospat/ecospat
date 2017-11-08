833c8b2e9920c4fd205ccea2792b9d40333f847e
Valeria Di Cola <valeria.dicola@unil.ch>
2017-11-08 15:20:51 +0100

    Update ecospat.exdet.r

--- ecospat/R/ecospat.exdet.r
+++ ecospat/R/ecospat.exdet.r
@@ -1,4 +1,4 @@
-####exdet_function###
+####climan_function###
 #### Coded by Blaise Petitpierre (bpetitpierre@gmail.com), 9th December 2015 after Mesgaran et al 2014
 # it allows to assess climate analogy between a projection extent (p) and a reference extent (ref, usend in general as the background to calibrate SDMS)
 # ref : a dataframe with the value of the variables (i.e columns) for each point of the reference exent
@@ -7,7 +7,7 @@
 # References: Mesgaran, M.B., Cousens, R.D. & Webber, B.L. (2014) Here be dragons: a tool for quantifying novelty due to covariate range and correlation change when projecting species distribution models. Diversity & Distributions, 20: 1147-1159, DOI: 10.1111/ddi.12209
 #
 
-ecospat.exdet<-function(ref,p){
+ecospat.climan<-function(ref,p){
 p<-as.matrix(p)
 a<-apply(ref,2,min);b<-apply(ref,2,max)
 minref<-matrix(a,nrow=nrow(p),ncol=ncol(p),byrow=T)
