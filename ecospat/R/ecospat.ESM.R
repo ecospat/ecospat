b6acc7c865b19b0eee4a4ec4759958ba7a22ab7c
fbreiner <frank.breiner@unil.ch>
2018-01-18 14:22:02 +0100

    Update ecospat.ESM.R

--- ecospat/R/ecospat.ESM.R
+++ ecospat/R/ecospat.ESM.R
@@ -883,7 +883,7 @@ ecospat.ESM.EnsembleProjection <- function(ESM.prediction.output, ESM.EnsembleMo
       weights.mod <- weights[grep(models[i], names(weights))]
       
       if(grepl(models[i], failed.mod)){
-      for(n in 1:sum(grepl(models[2], failed.mod))){
+      for(n in 1:sum(grepl(models[i], failed.mod))){
         weights.mod <- weights.mod[!grepl(paste(strsplit(grep(models[i] , failed.mod[n],value=T),split='_')[[1]][1],'\\b',sep=''),
              names(weights.mod))]
         }} ## end of loop removing failed models from the weighting vector
