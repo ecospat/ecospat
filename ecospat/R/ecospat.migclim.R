24f2f9e60e7c9e634538630ab5d8b7146c2bd83c
ecospat <40173384+ecospat@users.noreply.github.com>
2020-02-25 15:25:21 +0100

    Delete ecospat.migclim.R

--- ecospat/R/ecospat.migclim.R
+++
@@ -1,9 +0,0 @@
-ecospat.migclim <- function() {
-  message("load the MigClim package")
-  packageStartupMessage("initializing ...")
-  Sys.sleep(1)
-  requireNamespace("MigClim")
-  
-  Sys.sleep(1)
-  packageStartupMessage("... done")
-}
