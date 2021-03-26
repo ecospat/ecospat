1b493ad1b30d0f2f2455364b256806fb978405d1
Olivier Broennimann <Olivier.Broennimann@unil.ch>
2021-03-26 14:32:34 +0100

    nichePOSNB

---
+++ ecospat/R/ecospat.nichePOSNB.R
@@ -0,0 +1,46 @@
+
+nichePOSNB_2var<-function(df,colvar,colfreq){
+  
+  require(Hmisc)
+  var<-data.frame(df[,colvar])
+  freq<-data.frame(df[,colfreq])
+  
+  if (!is.numeric(colvar)||colvar==0||is.null(colvar)) stop("colvar should point to relevant columns in df corresponding to environmental axes")
+  if (!is.numeric(colfreq)||colfreq==0||is.null(colfreq)) stop("colfreq should point to relevant columns in df corresponding to taxa frequencies")
+  
+  if(ncol(var)==1) {
+    meanNicheByTaxa<-matrix(nrow=ncol(freq),ncol=2)
+    row.names(meanNicheByTaxa)<-colnames(freq)
+    colnames(var)<-colnames(df)[colvar]
+    colnames(meanNicheByTaxa)<- c(paste0(colnames(var),rep(c("_pos","_nb"))))
+  }
+  
+  if(ncol(var)!=1){
+    meanNicheByTaxa<-matrix(nrow=ncol(freq),ncol=(length(colvar)*2)+2)
+    row.names(meanNicheByTaxa)<-colnames(freq)
+    colnames(meanNicheByTaxa)<- c(paste0(rep(colnames(var),2),rep(c("_pos","_nb"),each=length(colvar))),"mean_pos","mean_nb")
+  }
+  
+  for (i in 1:nrow(meanNicheByTaxa)){
+    freq.i<-freq[,i]
+    freq.i<-(freq.i-min(freq.i))/(max(freq.i)-min(freq.i))
+    rangevar<-1:length(colvar)
+    for (j in rangevar){
+      meanNicheByTaxa[i,j]<- Hmisc::wtd.mean(var[,j],freq.i)
+      meanNicheByTaxa[i,j+length(colvar)] <- sqrt(Hmisc::wtd.var(var[,j],freq.i))
+    }
+    if(ncol(var)!=1){
+      meanNicheByTaxa[i,ncol(meanNicheByTaxa)-1] <- mean(meanNicheByTaxa[i,rangevar])
+      meanNicheByTaxa[i,ncol(meanNicheByTaxa)] <- mean(meanNicheByTaxa[i,rangevar+length(colvar)])
+    }
+  }
+  return(meanNicheByTaxa)
+}
+
+## test
+df <- read.delim("ecospat/data/ecospat.testNichePOSNB.txt")
+nichePOSNB_2var(df,colvar=c(2),colfreq = 4:15) # 1 axes
+nichePOSNB_2var(df,colvar=c(2:3),colfreq = 4:15) # 2 axes
+nichePOSNB_2var(df,colvar=c(2:6),colfreq = 4:15) # 5 axes
+
+
