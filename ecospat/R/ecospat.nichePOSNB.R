c24a8ae376979b80e2ba8d49ec64d7ef79ab36d8
Olivier Broennimann <Olivier.Broennimann@unil.ch>
2022-02-15 15:34:30 +0100

    split of nichePOSNB

--- ecospat/R/ecospat.nichePOSNB.R
+++ ecospat/R/ecospat.nichePOSNB.R
@@ -8,37 +8,37 @@ ecospat.nichePOSNB<-function(df,colvar,colfreq){
   var<-data.frame(df[,colvar])
   freq<-data.frame(df[,colfreq])
   
-  if(ncol(var)==1) {
-    meanNicheByTaxa<-matrix(nrow=ncol(freq),ncol=2)
-    row.names(meanNicheByTaxa)<-colnames(freq)
-    colnames(var)<-colnames(df)[colvar]
-    colnames(meanNicheByTaxa)<- c(paste0(colnames(var),rep(c("_pos","_nb"))))
-  }
+  POSNB<-matrix(nrow=ncol(freq),ncol=(length(colvar)*2))
+  row.names(POSNB)<-colnames(freq)
+  colnames(POSNB)<- c(paste0(rep(colnames(df)[colvar],2),rep(c("_pos","_nb"),each=length(colvar))))
   
-  if(ncol(var)!=1){
-    meanNicheByTaxa<-matrix(nrow=ncol(freq),ncol=(length(colvar)*2)+2)
-    row.names(meanNicheByTaxa)<-colnames(freq)
-    colnames(meanNicheByTaxa)<- c(paste0(rep(colnames(var),2),rep(c("_pos","_nb"),each=length(colvar))),"mean_pos","mean_nb")
-  }
-  
-  for (i in 1:nrow(meanNicheByTaxa)){
+  for (i in 1:ncol(freq)){
     freq.i<-freq[,i]
     freq.i<-(freq.i-min(freq.i))/(max(freq.i)-min(freq.i))
     rangevar<-1:length(colvar)
     for (j in rangevar){
-      meanNicheByTaxa[i,j]<- Hmisc::wtd.mean(var[,j],freq.i)
-      meanNicheByTaxa[i,j+length(colvar)] <- sqrt(Hmisc::wtd.var(var[,j],freq.i))
-    }
-    if(ncol(var)!=1){
-      meanNicheByTaxa[i,ncol(meanNicheByTaxa)-1] <- mean(meanNicheByTaxa[i,rangevar])
-      meanNicheByTaxa[i,ncol(meanNicheByTaxa)] <- mean(meanNicheByTaxa[i,rangevar+length(colvar)])
+      POSNB[i,j]<- Hmisc::wtd.mean(var[,j],freq.i)
+      POSNB[i,j+length(colvar)] <- sqrt(Hmisc::wtd.var(var[,j],freq.i))
     }
   }
-  return(meanNicheByTaxa)
+  return(POSNB)
 }
+
+ecospat.nicheNBmean<-function(POSNB,w=NULL){
+  if(ncol(POSNB)==2) stop("POSNB should have more than one axis")
+  NB<-POSNB[,(ncol(POSNB)/2+1):ncol(POSNB)]
+  if(is.null(w)) w<-rep(1/ncol(NB),ncol(NB))
+  if (!is.null(w)&length(w)!=ncol(NB)) stop("w should have the same length as the number of axes in POSNB")
+  NBmean<-NB%*%w/sum(w)
+  colnames(NBmean)<-"NBmean"
+  return(NBmean)
+  }
+
 
 ## test
 #df <- read.delim("ecospat/data/ecospat.testNichePOSNB.txt")
-#ecospat.nichePOSNB(df,colvar=c(2),colfreq = 4:15) # 1 axes
-#ecospat.nichePOSNB(df,colvar=c(2:3),colfreq = 4:15) # 2 axes
-#ecospat.nichePOSNB(df,colvar=c(2:6),colfreq = 4:15) # 5 axes
+#POSNB<-ecospat.nichePOSNB(df,colvar=c(2),colfreq = 4:15) # 1 axes
+#POSNB<-ecospat.nichePOSNB(df,colvar=c(2:3),colfreq = 4:15) # 2 axes
+#POSNB<-ecospat.nichePOSNB(df,colvar=c(2:5),colfreq = 4:15) # 4 axes
+#ecospat.nicheNBmean(POSNB,w=c(2,2))
+
