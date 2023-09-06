ec117d71acb4687c2a54571af482a8b20aea92b9
FlavienCollart <46991874+FlavienCollart@users.noreply.github.com>
2023-09-06 15:32:46 +0200

    Update ecospat.niche.projGeo.R
    Change to work with terra instead of raster

--- ecospat/R/ecospat.niche.projGeo.R
+++ ecospat/R/ecospat.niche.projGeo.R
@@ -2,8 +2,8 @@
 ## Written by Olivier Broennimann. Departement of Ecology and Evolution
 ## (DEE) & Institude of Earth Surface Dynamics
 ## University of Lausanne. Switzerland. April 2018.
-##
-## ecospat.niche.dynIndexProjGeo updated by Tyler Smith, April 2023
+## 
+## ecospat.niche.dynIndexProjGeo updated by Tyler Smith, April 2023 and Flavien Collart
 ##
 ## DESCRIPTION
 ##
@@ -26,13 +26,16 @@
 ## background (glob in ecospat.grid.clim.dyn)
 
 ecospat.niche.zProjGeo <- function(z1,env,cor=FALSE){
-
-  XY <- rasterToPoints(env)[,1:2] #geographical coordinates of each point of the background
+  if(inherits(env, "RasterStack") | inherits(env, "RasterBrick") |
+     inherits(env, "RasterLayer") ){
+    env <- terra::rast(env)
+  }
+  XY <- terra::crds(terra::as.points(env)) #geographical coordinates of each point of the background
 
   if (cor==FALSE) Z1<-extract(z1$z.uncor,z1$glob1) # occurrence density (niche) for each point of the background
   if (cor==TRUE) Z1<-extract(z1$z.cor,z1$glob1)
   XYZ1<-cbind(XY,Z1)
-  geoz1<-rasterFromXYZ(XYZ1)
+  geoz1<-terra::rast(XYZ1, type="xyz")
 
   return(geoz1)
 }
@@ -47,18 +50,22 @@ ecospat.niche.dynIndexProjGeo <- function(z1, z2, env) {
   ## raster.
 
   ## all env cells:
-  envVals <- raster::getValues(env)[,1]
+  if(inherits(env, "RasterStack") | inherits(env, "RasterBrick") |
+     inherits(env, "RasterLayer") ){
+    env <- terra::rast(env)
+  }
+  envVals <- terra::values(env)[,1]
 
   ## index values for the non-NA cells:
-  zCats <- raster::extract(zRast, z1$glob)
+  zCats <- terra::extract(zRast, z1$glob)[,1]
 
   ## add index values to the non-NA cells:
   envVals[!is.na(envVals)] <- zCats
   
-  ## create raster (without using values() which triggers namespace conflicts)
+  ## create grid (without using values() which triggers namespace conflicts)
 
-  zCatRast<-raster(matrix(envVals, nrow=env[[1]]@nrows, ncol=env[[1]]@ncols,byrow = TRUE),
-                   xmn=env@extent[1],xmx=env@extent[2],ymn=env@extent[3],ymx=env@extent[4])
+  zCatRast<-terra::rast(matrix(envVals, nrow=nrow(env[[1]]), ncol=ncol(env[[1]]),byrow = TRUE),
+                        extent = terra::ext(env))
   
   return(zCatRast)
 }
