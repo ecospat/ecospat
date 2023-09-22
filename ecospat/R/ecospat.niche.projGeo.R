c7039f42882cb0372457a5e68e10b6b66103d04f
ecospat <40173384+ecospat@users.noreply.github.com>
2023-09-22 12:01:43 +0200

    Update ecospat.niche.projGeo.R

--- ecospat/R/ecospat.niche.projGeo.R
+++ ecospat/R/ecospat.niche.projGeo.R
@@ -12,38 +12,48 @@
 ## 
 ## list of functions:
 ##
-## ecospat.niche.zProjGeo(z1,env,cor)
+## ecospat.niche.zProjGeo(z,zproj=NULL,env,cor=FALSE)
 ## projects the density of occurrence in space 
-## z1 is an object created by ecospat.grid.clim.dyn
-## env is a RasterStack of environmental variables corresponding to the background (glob in ecospat.grid.clim.dyn)
-## cor tells if the corrected or uncorrected occurrence density should be projected
-
-## ecospat.niche.dynIndexProjGeo(z1, z2, env)
-## projects the dynamic indexes ("stability", "unfilling" and "expansion")
-## in space
-## z1 and z2 are objects created by ecospat.grid.clim.dyn
+## z and zproj are objects created by ecospat.grid.clim.dyn
 ## env is a RasterStack of environmental variables corresponding to the
-## background (glob in ecospat.grid.clim.dyn)
+## background of z if zproj=NULL or zproj if not NULL (glob1 in ecospat.grid.clim.dyn)
+## cor tells if the corrected or uncorrected occurrence density should be projected
+##
+## ecospat.niche.dynIndexProjGeo(z1,z2,proj=0,env)
+## projects the dynamic indexes ("stability", "unfilling" and "expansion") in space
+## z1 and z2 are objects created by ecospat.grid.clim.dyn
+## proj indicate the type of projection: 0 for the global range, 1 for the range of z1, 2 for the range of z2
+## env is a RasterStack of environmental variables corresponding to the background (glob in ecospat.grid.clim.dyn
+## if proj = 0, glob1 if proj = 1 or proj = 2)
 
-ecospat.niche.zProjGeo <- function(z1,env,cor=FALSE){
+ecospat.niche.zProjGeo <- function(z,zproj=NULL,env,cor=FALSE){
   if(inherits(env, "RasterStack") | inherits(env, "RasterBrick") |
      inherits(env, "RasterLayer") ){
     env <- terra::rast(env)
   }
   XY <- terra::crds(terra::as.points(env)) #geographical coordinates of each point of the background
-
-  if (cor==FALSE) Z1<-extract(z1$z.uncor,z1$glob1) # occurrence density (niche) for each point of the background
-  if (cor==TRUE) Z1<-extract(z1$z.cor,z1$glob1)
-  XYZ1<-cbind(XY,Z1)
-  geoz1<-terra::rast(XYZ1, type="xyz")
-
-  return(geoz1)
+  
+  if(is.null(zproj)){
+    if(cor==FALSE) Z<-extract(z$z.uncor,z$glob1) # occurrence density (niche) for each point of the background
+    if(cor==TRUE) Z<-extract(z$z.cor,z$glob1)
+  }else{
+    if(cor==FALSE) Z<-extract(z$z.uncor,zproj$glob1) # occurrence density (niche) for each point of the background
+    if(cor==TRUE) Z<-extract(z$z.cor,zproj$glob1)
+  }
+  XYZ<-cbind(XY,Z)
+  geoz<-terra::rast(XYZ, type="xyz")
+  
+  return(geoz)
 }
 
 #########################
 
-ecospat.niche.dynIndexProjGeo <- function(z1, z2, env) {
+ecospat.niche.dynIndexProjGeo <- function(z1, z2, proj=0, env) {
   zRast <- z1$w + 2*z2$w
+  #0: outside of both niches
+  #1: only in z1 niche-> unfilling
+  #2: only in z2 niche-> expansion
+  #3: in both z1 and z2 niches -> stability
 
   ## We need **all** the cells from the env raster, even the NA ones. Those
   ## are not present in z1$glob or z2$glob, so we need the original env
@@ -57,7 +67,9 @@ ecospat.niche.dynIndexProjGeo <- function(z1, z2, env) {
   envVals <- terra::values(env)[,1]
 
   ## index values for the non-NA cells:
-  zCats <- terra::extract(zRast, z1$glob)[,1]
+  if(proj==0){ zCats <- terra::extract(zRast, z1$glob)}
+  if(proj==1){ zCats <- terra::extract(zRast, z1$glob1)}
+  if(proj==2){ zCats <- terra::extract(zRast, z2$glob1)}
 
   ## add index values to the non-NA cells:
   envVals[!is.na(envVals)] <- zCats
